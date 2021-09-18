28fce0387b029dcc7adb61f1dcbcaf08cd3e905e
Tobias Rødahl Thingnes <tobiasth@stud.ntnu.no>
2021-09-18 17:58:45 +0200

    Added native version of btinfo

--- android/app/src/main/kotlin/edu/ntnu/tobiasth/bluetooth_check/MainActivity.kt
+++ android/app/src/main/kotlin/edu/ntnu/tobiasth/bluetooth_check/MainActivity.kt
@@ -1,6 +1,32 @@
 package edu.ntnu.tobiasth.bluetooth_check
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.pm.PackageManager
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "tobiasth.ntnu.edu/bluetoothcheck";
+
+    override fun configureFlutterEngine(flutterEngine : FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "getBluetoothAvailable" -> {
+                    val hasBt = packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)
+                    val hasBtLe = packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)
+                    result.success(hasBt || hasBtLe)
+                }
+                "getBluetoothOn" -> {
+                    val btManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+                    val offStates = listOf(BluetoothAdapter.STATE_OFF, BluetoothAdapter.STATE_TURNING_OFF)
+                    result.success(btManager.adapter.state !in offStates)
+                }
+                else -> result.notImplemented()
+            }
+        }
+    }
 }
---
+++ lib/native_bluetooth_info.dart
@@ -0,0 +1,104 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class BluetoothInfo extends StatefulWidget {
+  const BluetoothInfo({required Key key}) : super(key: key);
+
+  @override
+  BluetoothInfoState createState() => BluetoothInfoState();
+}
+
+class BluetoothInfoState extends State<BluetoothInfo> {
+  static const platform = MethodChannel('tobiasth.ntnu.edu/bluetoothcheck');
+
+  late bool _btAvailable;
+  late bool _btOn;
+  String _stateInfo = 'No Bluetooth info has been retrieved yet.';
+  String _lastUpdated = '';
+
+  /// Gets Bluetooth availability from the relevant platform.
+  Future<bool> _getBluetoothAvailable() async {
+    final bool _available;
+
+    try {
+       _available = await platform.invokeMethod('getBluetoothAvailable');
+    } on PlatformException {
+      return false;
+    }
+
+    return _available;
+  }
+
+  /// Gets Bluetooth enabled status from the relevant platform.
+  Future<bool> _getBluetoothOn() async {
+    final bool _on;
+
+    try {
+      _on = await platform.invokeMethod('getBluetoothOn');
+    } on PlatformException {
+      return false;
+    }
+
+    return _on;
+  }
+
+  /// Updates the state widget once.
+  void updateState() async {
+    _btAvailable = await _getBluetoothAvailable();
+    _btOn = await _getBluetoothOn();
+
+    setState(() {
+      _stateInfo = _getStateInfoString();
+      _lastUpdated = _getLastUpdatedString();
+    });
+  }
+
+  /// Gets the current Bluetooth state string.
+  String _getStateInfoString() {
+    String _state = 'Bluetooth is';
+
+    if (_btAvailable) {
+      _state += ' available';
+
+      if (_btOn) {
+        _state += ' and turned on.';
+      } else {
+        _state += ', but turned off.';
+      }
+    } else {
+      _state += 'unavailable.';
+    }
+
+    return _state;
+  }
+
+  /// Gets the current last update string.
+  String _getLastUpdatedString() {
+    final _time = DateTime.now();
+    final _hour = _time.hour;
+    final _minute = _time.minute;
+    return 'Last updated at ' +
+        ((_hour < 10) ? '0' : '') +
+        _hour.toString() +
+        ((_minute < 10) ? ':0' : ':') +
+        _minute.toString();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          _stateInfo,
+          style: Theme.of(context).textTheme.subtitle1,
+          textAlign: TextAlign.center,
+        ),
+        Text(
+          _lastUpdated,
+        )
+      ],
+    );
+  }
+}
